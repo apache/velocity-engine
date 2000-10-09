b78d964b7a457657705dbfa0a6d3941142268426
Jason van Zyl <jvanzyl@apache.org>
2000-10-09 15:08:21 +0000

    - updates to reflect changes in nodes structure. fix a couple   of concurrency problems. PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/trunk@73241 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/velocity/runtime/directive/Directive.java
+++ src/java/org/apache/velocity/runtime/directive/Directive.java
@@ -7,14 +7,22 @@ import org.apache.velocity.Context;
 import org.apache.velocity.runtime.parser.Node;
 
 
-public interface Directive
+public abstract class Directive
 {
     public static final int BLOCK = 1;
     public static final int LINE = 2;
     
-    public String getName();
-    public int getType();
+    public abstract String getName();
+    public abstract int getType();
+    
+    public void init(Context context, Node node) throws Exception
+    {
+        int i, k = node.jjtGetNumChildren();
 
-    public void render(Context context, Writer writer, Node node)
+        for (i = 0; i < k; i++)
+            node.jjtGetChild(i).init(context, null);
+    }
+    
+    public abstract void render(Context context, Writer writer, Node node)
         throws IOException;
 }        
--- src/java/org/apache/velocity/runtime/directive/Foreach.java
+++ src/java/org/apache/velocity/runtime/directive/Foreach.java
@@ -14,87 +14,97 @@ import org.apache.velocity.util.ClassUtils;
 import org.apache.velocity.runtime.parser.Node;
 import org.apache.velocity.runtime.parser.Token;
 
-public class Foreach implements Directive
+public class Foreach extends Directive
 {
     public String getName() { return "foreach"; }        
     public int getType() { return BLOCK; }
 
+    private final static int ARRAY = 1;
+    private final static int ITERATOR = 2;
+
+    private String elementKey;
+    private Object listObject;
+    private Object tmp;
+    
+    public void init(Context context, Node node) throws Exception
+    {
+        Object sampleElement = null;
+        
+        elementKey = node.jjtGetChild(0).getFirstToken()
+                        .image.substring(1);
+        
+        // This is a refence node and it needs to
+        // be inititialized.
+        
+        node.jjtGetChild(2).init(context, null);
+        listObject = node.jjtGetChild(2).value(context);
+        
+        // Figure out what type of object the list
+        // element is so that we don't have to do it
+        // everytime the node is traversed.
+        
+        if (listObject == null)
+        {
+            System.out.println("dude you're fucked!");
+            System.out.println(node.jjtGetChild(2).getFirstToken().image);
+        }         
+        
+        if (listObject instanceof Object[])
+        {
+            node.setInfo(ARRAY);
+            sampleElement = ((Object[]) listObject)[0];
+        }            
+        else if (ClassUtils.implementsMethod(listObject, "iterator"))
+        {
+            node.setInfo(ITERATOR);
+            sampleElement = ((Collection) listObject).iterator().next();
+        }            
+    
+        // This is a little trick so that we can initialize
+        // all the blocks in the foreach  properly given
+        // that there are references that refer to the
+        // elementKey name.
+        
+        if (sampleElement != null)
+        {
+            context.put(elementKey, sampleElement);
+            super.init(context, node);
+            context.remove(elementKey);
+        }            
+    }
+
     public void render(Context context, Writer writer, Node node)
         throws IOException
     {
-    
-        // tokens 2,4
-        Object data = null;
-        Object listObject = null;
-        
-        // Now we have to iterate over all the child nodes
-        // for each in the list, we have to change the
-        // context each time the element changes.
-        String elementKey = node.jjtGetChild(0).getFirstToken()
-                                .image.substring(1);
-        
-        // if there is an object in the context with
-        // the same name as the $element save it so
-        // we can restore it after the #foreach.
-        
-        // The Collection interface provides iterator()
-        // I don't think elements() is provided by an interface
-        // it just looks like it belongs to Vector. Crappy
-        // 1.1 vs 1.2 junk.
-
-        Object tmp = null;
-        if (context.containsKey(elementKey))
-            tmp = context.get(elementKey);
-
         listObject = node.jjtGetChild(2).value(context);
         
-        /*!
-         * @desc Need to create a ReferenceException here, for
-         * example when the listObject is null. This
-         * obviously should be caught.
-         * @priority 1
-         */
-
-        if (listObject instanceof Object[])
+        switch(node.getInfo())
         {
-            int length = ((Object[]) listObject).length;
+            case ARRAY:
+                int length = ((Object[]) listObject).length;
             
-            for (int i = 0; i < length; i++)
-            {
-                context.put(elementKey,((Object[])listObject)[i]);
-                node.jjtGetChild(3).render(context, writer);
-            }
-            context.remove(elementKey);
-        }
-        else if (ClassUtils.implementsMethod(listObject, "iterator"))
-        {
-            // Maybe this could be optimized with get(index) ?
-            // Check the interface. size() and get(index) might
-            // be faster then using an Iterator.
-            Iterator i = ((Collection) listObject).iterator();
+                for (int i = 0; i < length; i++)
+                {
+                    context.put(elementKey,((Object[])listObject)[i]);
+                    node.jjtGetChild(3).render(context, writer);
+                }
+                context.remove(elementKey);
+                
+                break;
             
-            while (i.hasNext())
-            {
-                context.put(elementKey,i.next());
-                node.jjtGetChild(3).render(context, writer);
-            }
-            context.remove(elementKey);
-        }
-        else if (ClassUtils.implementsMethod(listObject, "elements"))
-        {
-            Enumeration e = ((Vector) listObject).elements();
+            case ITERATOR:
+                // Maybe this could be optimized with get(index) ?
+                // Check the interface. size() and get(index) might
+                // be faster then using an Iterator.
+                Iterator i = ((Collection) listObject).iterator();
             
-            while (e.hasMoreElements())
-            {
-                context.put(elementKey,e.nextElement());
-                node.jjtGetChild(3).render(context, writer);
-            }
-            context.remove(elementKey);
-        }
-
-        // Restore the element that was overridden by
-        // the iterator.
-        if (tmp != null)
-            context.put(elementKey, tmp);
+                while (i.hasNext())
+                {
+                    context.put(elementKey,i.next());
+                    node.jjtGetChild(3).render(context, writer);
+                }
+                context.remove(elementKey);
+                break;
+        }            
     }
 }
