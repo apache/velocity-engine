7051196e0ccd19b0e5761f6fb603b991724d4709
Geir Magnusson Jr <geirm@apache.org>
2000-11-02 15:28:37 +0000

    Fixed Example.java and commented it.  Also the .vm didn't reflect the new #foo() syntax for directives.  Also, updated the properties file (took the one from test/misc). PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/trunk@73523 13f79535-47bb-0310-9956-ffa450edef68

--- examples/test.sh
+++ examples/test.sh
@@ -1,1 +1,5 @@
-java -cp .:../velocity.jar Example example.vm > output
+echo "Running Example with input file 'example.vm'"
+
+java -cp .:../bin/velocity-0.4.jar Example example.vm  > output
+
+echo "Output of Example is in the file 'output'"
