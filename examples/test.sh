276cb9e7aefa32189d3db6311ec18c2ac3358e71
jcastura <jcastura@unknown>
2000-12-02 05:54:44 +0000

    velocity jar now 0.7
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/trunk@73847 13f79535-47bb-0310-9956-ffa450edef68

--- examples/test.sh
+++ examples/test.sh
@@ -1,5 +1,5 @@
 echo "Running Example with input file 'example.vm'"
 
-java -cp .:../bin/velocity-0.4.jar Example example.vm  > output
+java -cp .:../bin/velocity-0.7.jar Example example.vm  > output
 
 echo "Output of Example is in the file 'output'"
