8fa3c29f8c93130b2439dd34d6fc47891bc6b764
Geir Magnusson Jr <geirm@apache.org>
2000-12-19 13:35:20 +0000

    Parser.jjt followers PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/trunk@73974 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/velocity/runtime/parser/Parser.java
+++ src/java/org/apache/velocity/runtime/parser/Parser.java
@@ -21,7 +21,7 @@ import org.apache.velocity.util.StringUtils;
  *
  * @author <a href="mailto:jvanzyl@periapt.com">Jason van Zyl</a>
  * @author <a href="mailto:geirm@optonline.net">Geir Magnusson Jr.</a>
- * @version $Id: Parser.java,v 1.43 2000/12/19 11:47:37 geirm Exp $ 
+ * @version $Id: Parser.java,v 1.44 2000/12/19 13:35:18 geirm Exp $ 
 */
 public class Parser/*@bgen(jjtree)*/implements ParserTreeConstants, ParserConstants {/*@bgen(jjtree)*/
   protected JJTParserState jjtree = new JJTParserState();/**
