ac7c5c36be7b67edb769506115c5a75ba5910832
Geir Magnusson Jr <geirm@apache.org>
2001-01-03 05:13:58 +0000

    Replacement for the Context.java class.   This is the primary Context implementation to be used by applications.  Supports chaining. PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/trunk@74070 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/velocity/VelocityContext.java
@@ -0,0 +1,147 @@
+package org.apache.velocity;
+
+/*
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2000 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Velocity", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+import java.util.HashMap;
+
+import org.apache.velocity.context.AbstractContext;
+import org.apache.velocity.context.Context;
+
+/**
+ *  General purpose implemention of the application Context 
+ *  interface for general application use.  This class should 
+ *  be used in place of the original Context class.
+ *
+ *  This implementation uses a HashMap  (@see java.util.HashMap ) 
+ *  for data storage.
+ *
+ *  This context implementation cannot be shared between threads
+ *  without those threads synchronizing access between them, as 
+ *  the HashMap is not synchronized, nor are some of the fundamentals
+ *  of AbstractContext.  If you need to share a Context between 
+ *  threads with simultaneous access for some reason, please create 
+ *  your own and extend the interface Context 
+ *  
+ *  @see org.apache.velocity.context.Context
+ *
+ *  @author <a href="mailto:geirm@optonline.net">Geir Magnusson Jr.</a>
+ *  @author <a href="mailto:jvanzyl@periapt.com">Jason van Zyl</a>
+ *  @author <a href="mailto:fedor.karpelevitch@home.com">Fedor Karpelevitch</a>
+ *  @version $Id: VelocityContext.java,v 1.1 2001/01/03 05:13:58 geirm Exp $
+ */
+
+public class VelocityContext extends AbstractContext implements Cloneable
+{
+    /** storage for key/value pairs */
+    private HashMap context = new HashMap();
+
+    /** default CTOR */
+    public VelocityContext()
+    {
+        super();
+    }
+
+    /** chaining CTOR */
+    public VelocityContext( Context inner )
+    {
+        super( inner );
+    }
+ 
+    public Object internalGet( String key )
+    {
+        return context.get( key );
+    }        
+
+    public Object internalPut( String key, Object value )
+    {
+        return context.put( key, value );
+    }
+
+    public  boolean internalContainsKey(Object key)
+    {
+        return context.containsKey( key );
+    }
+
+    public  Object[] internalGetKeys()
+    {
+        return context.keySet().toArray();
+    }
+
+    public  Object internalRemove(Object key)
+    {
+        return context.remove( key );
+    }
+
+    /**
+     * Clones this context object
+     * @return Object an instance of this Context
+     */
+    public Object clone()
+    {
+        VelocityContext clone = null;
+
+        try
+        {
+            clone = (VelocityContext) super.clone();
+            clone.context = (HashMap) context.clone();
+        }
+        catch (CloneNotSupportedException cnse)
+        {
+        }
+        return clone;
+    }
+
+}
+
