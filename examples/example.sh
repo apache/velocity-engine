5dbdf95d73ab4d6c39faaeb0f85bf24b1dced758
Geir Magnusson Jr <geirm@apache.org>
2001-02-12 03:06:30 +0000

    Removing all in one shot (the one-by-one got boring...)
    Will be adding all source, shells, and example templates back
    into separate directories - I think this will be better for
    firstime users to keep things simple.  Class files will no
    longer be distributed - the will be generated by the build
    process
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/trunk@74237 13f79535-47bb-0310-9956-ffa450edef68

--- examples/example.sh
+++
@@ -1,9 +0,0 @@
-echo "Running Example with input file 'example.vm'"
-
-for i in ../bin/*.jar
-do
-    _VELCP=$VELCP:"$i"
-done
-
-java -cp $_VELCP:. Example example.vm  
-
--- examples/example2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for i in ../bin/*.jar
-do
-    _VELCP=$VELCP:"$i"
-done
-
-java -cp $_VELCP Example2
-
