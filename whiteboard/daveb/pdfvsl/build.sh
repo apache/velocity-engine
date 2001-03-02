2369b96f22b1de9c88f0a0e689a73328de54cef6
daveb <daveb@unknown>
2001-03-02 04:15:42 +0000

    Start of anakia generating PDF.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/trunk@74323 13f79535-47bb-0310-9956-ffa450edef68

---
+++ whiteboard/daveb/pdfvsl/build.sh
@@ -0,0 +1,39 @@
+#! /bin/sh
+
+if [ "$JAVA_HOME" = "" ] ; then
+  echo You must set JAVA_HOME to point at your Java Development Kit directory
+  exit 1
+fi
+
+# convert the existing path to unix
+if [ "$OSTYPE" = "cygwin32" ] || [ "$OSTYPE" = "cygwin" ] ; then
+   CLASSPATH=`cygpath --path --unix "$CLASSPATH"`
+fi
+
+CLASSPATH=${JAVA_HOME}/lib/tools.jar
+
+for i in ../../../build/lib/*.jar
+do
+    CLASSPATH=$CLASSPATH:"$i"
+done
+
+for i in ../../../bin/*.jar
+do
+    CLASSPATH=$CLASSPATH:"$i"
+done
+
+# convert the unix path to windows
+if [ "$OSTYPE" = "cygwin32" ] || [ "$OSTYPE" = "cygwin" ] ; then
+   CLASSPATH=`cygpath --path --windows "$CLASSPATH"`
+fi
+
+CLASSPATH=${CLASSPATH}:../../../build/lib
+
+BUILDFILE=build.xml
+
+echo $CLASSPATH
+
+java $ANT_OPTS -classpath "$CLASSPATH" org.apache.tools.ant.Main \
+                -Dant.home=$ANT_HOME \
+                -buildfile ${BUILDFILE} \
+                 "$@"
