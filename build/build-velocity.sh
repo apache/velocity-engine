9b60d020f373484d5d20de27533050788951a5fe
Jon Scott Stevens <jon@apache.org>
2001-03-11 21:49:11 +0000

    ant 1.3
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/trunk@74413 13f79535-47bb-0310-9956-ffa450edef68

--- build/build-velocity.sh
+++ build/build-velocity.sh
@@ -24,8 +24,8 @@ if [ "$OSTYPE" = "cygwin32" ] || [ "$OSTYPE" = "cygwin" ] ; then
    CLASSPATH=`cygpath --path --unix "$CLASSPATH"`
 fi
 
-CLASSPATH=${CLASSPATH}:./lib/ant-1.2.jar
-CLASSPATH=${CLASSPATH}:./lib/ant-1.2-optional.jar
+CLASSPATH=${CLASSPATH}:./lib/ant-1.3.jar
+CLASSPATH=${CLASSPATH}:./lib/ant-1.3-optional.jar
 CLASSPATH=${CLASSPATH}:./lib/xerces-1.2.1.jar
 CLASSPATH=${CLASSPATH}:./lib/werken.xpath.jar
 CLASSPATH=${CLASSPATH}:./lib/antlr-runtime.jar
