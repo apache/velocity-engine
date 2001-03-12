df5a7933b4094181eebb90ca86e64c3a019d874a
Jon Scott Stevens <jon@apache.org>
2001-03-12 01:25:51 +0000

    merged the stuff in misc/Test.java into the TemplateTestCase and the new VelocityAppTestCase. Note: still need to implement more tests for the app.Velocity class.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/trunk@74432 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/velocity/test/VelocityAppTestCase.java
@@ -0,0 +1,195 @@
+package org.apache.velocity.test;
+
+/*
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Velocity", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.StringWriter;
+
+import java.util.Vector;
+
+import org.apache.velocity.VelocityContext;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.test.provider.TestProvider;
+import org.apache.velocity.runtime.Runtime;
+import org.apache.velocity.util.StringUtils;
+
+import org.apache.velocity.app.Velocity;
+
+import junit.framework.TestCase;
+
+/**
+ * This class is intended to test the app.Velocity.java class.
+ *
+ * @author <a href="mailto:geirm@optonline.net">Geir Magnusson Jr.</a>
+ * @author <a href="mailto:jon@latchkey.com">Jon S. Stevens</a>
+ * @version $Id: VelocityAppTestCase.java,v 1.1 2001/03/12 01:25:50 jon Exp $
+ */
+public class VelocityAppTestCase extends TestCase implements TemplateTestBase
+{
+    private StringWriter compare1 = new StringWriter();
+    private String input1 = "My name is $name -> $Floog";
+    private String result1 = "My name is jason -> floogie woogie";
+
+    public VelocityAppTestCase()
+    {
+        super("VelocityAppTestCase");
+        try
+        {
+	        Runtime.setDefaultProperties();
+	        Runtime.setSourceProperty(
+	            Runtime.FILE_RESOURCE_LOADER_PATH, FILE_RESOURCE_LOADER_PATH);
+	        Runtime.init();
+	    }
+	    catch (Exception e)
+	    {
+            System.err.println("Cannot setup VelocityAppTestCase!");
+            e.printStackTrace();
+            System.exit(1);
+	    }
+    }
+
+    public static junit.framework.Test suite()
+    {
+        return new VelocityAppTestCase();
+    }
+
+    /**
+     * Runs the test.
+     */
+    public void runTest ()
+    {
+        VelocityContext context = new VelocityContext();
+        context.put("name", "jason");
+        context.put("Floog", "floogie woogie");
+
+        try
+        {
+            Velocity.evaluate(context, compare1, "evaltest", input1);
+
+/*
+            FIXME: Not tested right now.
+            
+            StringWriter result2 = new StringWriter();
+            Velocity.mergeTemplate("mergethis.vm",  context, result2);
+
+            StringWriter result3 = new StringWriter();
+            Velocity.invokeVelocimacro("floog", "test", new String[2], 
+                                        context, result3);
+*/
+            if (!isOutputCorrect())
+            {
+                fail("Output incorrect.");
+            }
+        }
+        catch (Exception e)
+        {
+            fail(e.getMessage());
+        }
+    }
+
+    /**
+     * Returns whether the processed template matches the content of the 
+     * provided comparison file.
+     *
+     * @return Whether the output matches the contents of the comparison file.
+     *
+     * @exception Exception Test failure condition.
+     */
+    protected boolean isOutputCorrect() throws Exception
+    {
+        return ( result1.equals(compare1.toString()));
+    }
+
+    /**
+     * Concatenates the file name parts together appropriately.
+     *
+     * @return The full path to the file.
+     */
+    private static String getFileName (String dir, String base, String ext)
+    {
+        StringBuffer buf = new StringBuffer();
+        if (dir != null)
+        {
+            buf.append(dir).append('/');
+        }
+        buf.append(base).append('.').append(ext);
+        return buf.toString();
+    }
+
+    /**
+     * Turns a base file name into a test case name.
+     *
+     * @param s The base file name.
+     * @return  The test case name.
+     */
+    private static final String getTestCaseName (String s)
+    {
+        StringBuffer name = new StringBuffer();
+        name.append(Character.toTitleCase(s.charAt(0)));
+        name.append(s.substring(1, s.length()).toLowerCase());
+        return name.toString();
+    }
+
+    /**
+     * Performs cleanup activities for this test case.
+     */
+    protected void tearDown () throws Exception
+    {
+        // No op.
+    }
+}
