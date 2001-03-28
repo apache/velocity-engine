d2be036e9ac0c3044a5d56e7eb1cb03567102c70
Geir Magnusson Jr <geirm@apache.org>
2001-03-28 03:17:27 +0000

    Added license and javadoc indicating why some of the functions are just stubs. PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/trunk@74738 13f79535-47bb-0310-9956-ffa450edef68

--- examples/context_example/DBContext.java
+++ examples/context_example/DBContext.java
@@ -1,3 +1,56 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2000-2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Velocity", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
 
 import java.sql.*;
 import java.io.Serializable;
@@ -21,7 +74,7 @@ import org.apache.velocity.context.Context;
  *  very fragile, crappy code.... just a demo!
  *
  * @author <a href="mailto:geirm@optonline.net">Geir Magnusson Jr.</a>
- * @version $Id: DBContext.java,v 1.1 2001/02/12 03:11:25 geirm Exp $
+ * @version $Id: DBContext.java,v 1.2 2001/03/28 03:17:27 geirm Exp $
  */
 
 public class DBContext extends AbstractContext
@@ -40,6 +93,11 @@ public class DBContext extends AbstractContext
         setup();
     }
 
+    /**
+     *  retrieves a serialized object from the db
+     *  and returns the living instance to the
+     *  caller.
+     */
     public Object internalGet( String key )
     {
         try 
@@ -74,9 +132,14 @@ public class DBContext extends AbstractContext
         return null;
     }
 
+    /**
+     *  Serializes and stores an object in the database.
+     *  This is really a hokey way to do it, and will
+     *  cause problems.  The right way is to use a 
+     *  prepared statement...
+     */
     public Object internalPut( String key, Object value )
     {
-
         try 
         {    
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -103,16 +166,31 @@ public class DBContext extends AbstractContext
         return null;
     }
 
+    /**
+     *  Not implementing. Not required for Velocity core
+     *  operation, so not bothering.  As we say above :
+     *  "very fragile, crappy code..."
+     */
     public  boolean internalContainsKey(Object key)
     {
         return false;
     }
-
+    
+    /**
+     *  Not implementing. Not required for Velocity core
+     *  operation, so not bothering.  As we say above :
+     *  "very fragile, crappy code..."
+     */
     public  Object[] internalGetKeys()
     {
         return null;
     }
 
+    /**
+     *  Not implementing. Not required for Velocity core
+     *  operation, so not bothering.  As we say above :
+     *  "very fragile, crappy code..."
+     */
     public  Object internalRemove(Object key)
     {
         return null;
