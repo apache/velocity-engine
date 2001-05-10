01e6f0004c548811f105107711e60c5c4e836b13
Geir Magnusson Jr <geirm@apache.org>
2001-05-10 10:56:49 +0000

    Cygwin patches from Richard Hensley <Richard.Hensley@itb.mckhboc.com>
    Thanks!
    
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/trunk@74941 13f79535-47bb-0310-9956-ffa450edef68

--- examples/xmlapp_example/xmlapp_example.sh
+++ examples/xmlapp_example/xmlapp_example.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+_VELCP=.
+
 for i in ../../build/lib/*.jar
 do
     _VELCP=$_VELCP:"$i"
@@ -10,6 +12,10 @@ do
     _VELCP=$_VELCP:"$i"
 done
 
+# convert the unix path to windows
+if [ "$OSTYPE" = "cygwin32" ] || [ "$OSTYPE" = "cygwin" ] ; then
+    _VELCP=`cygpath --path --windows "$_VELCP"`
+fi
 
 java -cp $_VELCP XMLTest xml.vm
 
