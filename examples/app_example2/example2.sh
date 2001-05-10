e84b772fe16da85290062e1377afb868c0244cc8
Geir Magnusson Jr <geirm@apache.org>
2001-05-10 10:56:49 +0000

    Cygwin patches from Richard Hensley <Richard.Hensley@itb.mckhboc.com>
    Thanks!
    
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/trunk@74941 13f79535-47bb-0310-9956-ffa450edef68

--- examples/app_example2/example2.sh
+++ examples/app_example2/example2.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+_VELCP=.
+
 for i in ../../bin/*.jar
 do
     _VELCP=$_VELCP:"$i"
 done
+ 
+# convert the unix path to windows
+if [ "$OSTYPE" = "cygwin32" ] || [ "$OSTYPE" = "cygwin" ] ; then
+    _VELCP=`cygpath --path --windows "$_VELCP"`
+fi
 
 java -cp $_VELCP Example2
-
+ 
--- examples/logger_example/logger_example.sh
+++ examples/logger_example/logger_example.sh
@@ -1,9 +1,16 @@
 echo "Running LoggerExample:"
 
+_VELCP=.
+
 for i in ../../bin/*.jar
 do
     _VELCP=$_VELCP:"$i"
 done
+ 
+# convert the unix path to windows
+if [ "$OSTYPE" = "cygwin32" ] || [ "$OSTYPE" = "cygwin" ] ; then
+    _VELCP=`cygpath --path --windows "$_VELCP"`
+fi
 
-java -cp $_VELCP:. LoggerExample 
+java -cp $_VELCP LoggerExample 
 
