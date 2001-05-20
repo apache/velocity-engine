ebaf624d9747acbbc705ebe6cc913dbf1fc548f3
Geir Magnusson Jr <geirm@apache.org>
2001-05-20 20:30:37 +0000

    Copy of the Configuration test case w/ the Commons' ExtendedProperties substituted, as this is what we are going to use going forward. PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/trunk@75008 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/velocity/test/CommonsExtPropTestCase.java
@@ -0,0 +1,234 @@
+package org.apache.velocity.test;
+
+/*
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Velocity", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+import java.io.File;
+import java.io.FileWriter;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.util.StringUtils;
+
+import org.apache.commons.collections.ExtendedProperties;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests for the Commons ExtendedProperties class. This is an identical
+ *  copy of the ConfigurationTestCase, which will disappear when
+ *  the Configuration class does
+ *
+ * @author <a href="mailto:jvanzyl@apache.org">Jason van Zyl</a>
+ * @author <a href="mailto:geirm@optonline.net">Geir Magnusson Jr.</a>
+ * @version $Id: CommonsExtPropTestCase.java,v 1.1 2001/05/20 20:30:37 geirm Exp $
+ */
+public class CommonsExtPropTestCase extends BaseTestCase
+{
+    /**
+     * Comparison directory.
+     */
+    private static final String COMPARE_DIR = 
+        "../test/configuration/compare";
+    
+    /**
+     * Results directory.
+     */
+    private static final String RESULTS_DIR = 
+        "../test/configuration/results";
+
+    /**
+     * Test configuration
+     */
+    private static final String TEST_CONFIG = 
+        "../test/configuration/test.config";
+
+    /**
+     * Creates a new instance.
+     *
+     */
+    public CommonsExtPropTestCase()
+    {
+        super("CommonsExtPropTestCase");
+    }
+
+    public static junit.framework.Test suite()
+    {
+        return new CommonsExtPropTestCase();
+    }
+
+    /**
+     * Runs the test.
+     */
+    public void runTest ()
+    {
+        try
+        {
+            assureResultsDirectoryExists(RESULTS_DIR);
+            
+            ExtendedProperties c = new ExtendedProperties(TEST_CONFIG);
+            
+            FileWriter result = new FileWriter(
+                getFileName(RESULTS_DIR, "output", "res"));
+            
+            message(result, "Testing order of keys ...");
+            showIterator(result, c.getKeys());
+            
+            message(result, "Testing retrieval of CSV values ...");
+            showVector(result, c.getVector("resource.loader"));            
+
+            message(result, "Testing subset(prefix).getKeys() ...");
+            ExtendedProperties subset = c.subset("file.resource.loader");
+            showIterator(result, subset.getKeys());
+
+            message(result, "Testing getVector(prefix) ...");
+            showVector(result, subset.getVector("path"));            
+
+            message(result, "Testing getString(key) ...");
+            result.write(c.getString("config.string.value"));
+            result.write("\n\n");
+
+            message(result, "Testing getBoolean(key) ...");
+            result.write(new Boolean(c.getBoolean("config.boolean.value")).toString());
+            result.write("\n\n");
+
+            message(result, "Testing getByte(key) ...");
+            result.write(new Byte(c.getByte("config.byte.value")).toString());
+            result.write("\n\n");
+
+            message(result, "Testing getShort(key) ...");
+            result.write(new Short(c.getShort("config.short.value")).toString());
+            result.write("\n\n");
+
+            message(result, "Testing getInt(key) ...");
+            result.write(new Integer(c.getInt("config.int.value")).toString());
+            result.write("\n\n");
+
+            message(result, "Testing getLong(key) ...");
+            result.write(new Long(c.getLong("config.long.value")).toString());
+            result.write("\n\n");
+
+            message(result, "Testing getFloat(key) ...");
+            result.write(new Float(c.getFloat("config.float.value")).toString());
+            result.write("\n\n");
+
+            message(result, "Testing getDouble(key) ...");
+            result.write(new Double(c.getDouble("config.double.value")).toString());
+            result.write("\n\n");
+
+            message(result, "Testing escaped-comma scalar...");
+            result.write( c.getString("escape.comma1"));
+            result.write("\n\n");
+
+            message(result, "Testing escaped-comma vector...");
+            showVector(result,  c.getVector("escape.comma2"));
+            result.write("\n\n");
+
+            result.flush();
+            result.close();
+            
+            if (!isMatch(RESULTS_DIR, COMPARE_DIR, "output","res","cmp"))
+            {
+                fail("Output incorrect.");
+            }
+        }
+        catch (Exception e)
+        {
+            System.err.println("Cannot setup CommonsExtPropTestCase!");
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private void showIterator(FileWriter result, Iterator i)
+        throws Exception
+    {
+        while(i.hasNext())
+        {
+            result.write((String) i.next());
+            result.write("\n");
+        }
+        result.write("\n");
+    }
+
+    private void showVector(FileWriter result, Vector v)
+        throws Exception
+    {
+        for (int j = 0; j < v.size(); j++)
+        {
+            result.write((String) v.get(j));
+            result.write("\n");
+        }
+        result.write("\n");
+    }
+
+    private void message(FileWriter result, String message)
+        throws Exception
+    {
+        result.write("--------------------------------------------------\n");
+        result.write(message + "\n");
+        result.write("--------------------------------------------------\n");
+        result.write("\n");
+    }
+}
+
+
+
+
+
+
+
+
