fcd4bf58df2ebf3ab0d2499db42045afafe2762d
Jon Scott Stevens <jon@apache.org>
2001-08-08 17:33:08 +0000

    added notes to changes doc updated test.sh script to just use ant PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/trunk@75171 13f79535-47bb-0310-9956-ffa450edef68

--- build/test.sh
+++ build/test.sh
@@ -8,37 +8,4 @@
 #--------------------------------------------
 # No need to edit anything past here
 #--------------------------------------------
-if test -z "${JAVA_HOME}" ; then
-    echo "ERROR: JAVA_HOME not found in your environment."
-    echo "Please, set the JAVA_HOME variable in your environment to match the"
-    echo "location of the Java Virtual Machine you want to use."
-    exit
-fi
-
-if test -f ${JAVA_HOME}/lib/tools.jar ; then
-    CLASSPATH="${CLASSPATH}:${JAVA_HOME}/lib/tools.jar"
-fi
-
-# convert the existing path to unix
-if [ "$OSTYPE" = "cygwin32" ] || [ "$OSTYPE" = "cygwin" ] ; then
-    CLASSPATH=`cygpath --path --unix "$CLASSPATH"`
-fi
-
-CLASSPATH=${CLASSPATH}:./lib/ant-1.3.jar
-CLASSPATH=${CLASSPATH}:./lib/ant-1.3-optional.jar
-CLASSPATH=${CLASSPATH}:./lib/xerces-1.3.0.jar
-CLASSPATH=${CLASSPATH}:./lib/werken.xpath.jar
-CLASSPATH=${CLASSPATH}:./lib/antlr-runtime.jar
-
-# convert the unix path to windows
-if [ "$OSTYPE" = "cygwin32" ] || [ "$OSTYPE" = "cygwin" ] ; then
-    CLASSPATH=`cygpath --path --windows "$CLASSPATH"`
-fi
-
-if [ "$BUILDFILE" = "" ] ; then
-    BUILDFILE=testcases.xml
-fi
-
-${JAVA_HOME}/bin/java -classpath ${CLASSPATH} \
-                       org.apache.tools.ant.Main \
-                       -buildfile ${BUILDFILE} "$@"
+ant -buildfile testcases.xml "$@"
