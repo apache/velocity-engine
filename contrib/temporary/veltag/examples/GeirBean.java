41c2c5d4a3f3e32d4bde49121e060da3ca8e2246
Geir Magnusson Jr <geirm@apache.org>
2001-08-14 00:07:39 +0000

    Initial checking for Velocity JSP tag
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/trunk@75199 13f79535-47bb-0310-9956-ffa450edef68

---
+++ contrib/temporary/veltag/examples/GeirBean.java
@@ -0,0 +1,19 @@
+
+public class GeirBean
+{
+    private static String[] arr = { "a","b","c","d"};
+
+    public void setString( String foo )
+    {
+    }
+
+    public String getString()
+    {
+        return "Hello from GeirBean";
+    }
+
+    public String[] getArray()
+    {
+        return arr;
+    }
+}
