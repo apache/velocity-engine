e87fc08b9a4a430246f44d6fd414079ccf79ec23
Geir Magnusson Jr <geirm@apache.org>
2001-12-10 12:06:58 +0000

    updates from 1.2 to HEAD PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/trunk@75511 13f79535-47bb-0310-9956-ffa450edef68

--- examples/app_example2/example2.sh
+++ examples/app_example2/example2.sh
@@ -7,6 +7,11 @@ do
     _VELCP=$_VELCP:"$i"
 done
  
+for i in ../../build/lib/*.jar
+do
+    _VELCP=$_VELCP:"$i"
+done
+ 
 # convert the unix path to windows
 if [ "$OSTYPE" = "cygwin32" ] || [ "$OSTYPE" = "cygwin" ] ; then
     _VELCP=`cygpath --path --windows "$_VELCP"`
