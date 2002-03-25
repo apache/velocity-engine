c3ba7b2a92370714d6a40de3364219f3e8791717
Geir Magnusson Jr <geirm@apache.org>
2002-03-25 00:47:30 +0000

    Fixes for 7396 - there is no reason why we shouldn't be able to do
      #set($foo = ( 3 == 3 ))
    
    there is now real downside - we allow any other kind of expression...
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/trunk@75642 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/velocity/runtime/parser/node/ASTGTNode.java
+++ src/java/org/apache/velocity/runtime/parser/node/ASTGTNode.java
@@ -122,4 +122,13 @@ public class ASTGTNode extends SimpleNode
 
         return ((Integer) left).intValue() > ((Integer) right).intValue();
     }
+
+    public Object value(InternalContextAdapter context)
+        throws MethodInvocationException
+    {
+        boolean val = evaluate(context);
+
+        return val ? Boolean.TRUE : Boolean.FALSE;
+    }
+
 }
