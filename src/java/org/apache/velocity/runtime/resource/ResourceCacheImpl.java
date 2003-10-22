41de89fed79b064d465bf118881cf0fa445f2f3b
Daniel Rall <dlr@apache.org>
2003-10-22 03:00:46 +0000

    * src/java/org/apache/velocity/runtime/resource/ResourceCacheImpl.java   initialize(RuntimeServices): Leveraged LRUMap from Commons   Collections.  If the resource.manager.cache.size property is set to   0 or less, the previous greedy behavior is available.
    * src/java/org/apache/velocity/runtime/RuntimeConstants.java
      RESOURCE_MANAGER_CACHE_CLASS: Added JavaDoc.
    
      RESOURCE_MANAGER_CACHE_SIZE: New constant for the resource cache
      size property.
    
    
    * xdocs/developer-guide.xml
    * docs/developer-guide.html
      Mentioned the RESOURCE_MANAGER_CACHE_SIZE
      (resource.manager.cache.size) property.
    
    
    * xdocs/changes.xml
    * docs/changes.html
      Noted new LRUMap implementation of ResourceCacheImpl.
    
    
    * TODO.txt
      Removed the "Control Caching Resource Usage" item.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/trunk@75845 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/velocity/runtime/resource/ResourceCacheImpl.java
+++ src/java/org/apache/velocity/runtime/resource/ResourceCacheImpl.java
@@ -54,18 +54,28 @@ package org.apache.velocity.runtime.resource;
  * <http://www.apache.org/>.
  */
 
+import java.util.Collections;
 import java.util.Hashtable;
 import java.util.Map;
 import java.util.Iterator;
+
+import org.apache.commons.collections.LRUMap;
+import org.apache.velocity.runtime.RuntimeConstants;
 import org.apache.velocity.runtime.RuntimeServices;
 
 /**
  * Default implementation of the resource cache for the default
- * ResourceManager.
+ * ResourceManager.  The cache uses a <i>least recently used</i> (LRU)
+ * algorithm, with a maximum size specified via the
+ * <code>resource.manager.cache.size</code> property (idenfied by the
+ * {@link
+ * org.apache.velocity.runtime.RuntimeConstants#RESOURCE_MANAGER_CACHE_SIZE}
+ * constant).  This property get be set to <code>0</code> or less for
+ * a greedy, unbounded cache (the behavior from pre-v1.5).
  *
  * @author <a href="mailto:geirm@apache.org">Geir Magnusson Jr.</a>
  * @author <a href="mailto:dlr@finemaltcoding.com">Daniel Rall</a>
- * @version $Id: ResourceCacheImpl.java,v 1.2 2002/02/12 19:08:06 dlr Exp $
+ * @version $Id: ResourceCacheImpl.java,v 1.3 2003/10/22 03:00:46 dlr Exp $
  */
 public class ResourceCacheImpl implements ResourceCache
 {
@@ -83,8 +93,18 @@ public class ResourceCacheImpl implements ResourceCache
     public void initialize( RuntimeServices rs )
     {
         rsvc = rs;
-        
-        rsvc.info("ResourceCache : initialized. (" + this.getClass() + ")");
+
+        int maxSize =
+            rsvc.getInt(RuntimeConstants.RESOURCE_MANAGER_CACHE_SIZE, 89);
+        if (maxSize > 0)
+        {
+            // Create a whole new Map here to avoid hanging on to a
+            // handle to the unsynch'd LRUMap for our lifetime.
+            Map lruCache = Collections.synchronizedMap(new LRUMap(maxSize));
+            lruCache.putAll(cache);
+            cache = lruCache;
+        }
+        rsvc.info("ResourceCache : initialized. (" + this.getClass() + ')');
     }
     
     public Resource get( Object key )
