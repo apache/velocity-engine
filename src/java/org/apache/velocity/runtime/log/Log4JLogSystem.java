9ea2b3722875f57fbe2d9f7effd5ba2a89b4a86e
Geir Magnusson Jr <geirm@apache.org>
2003-10-22 10:49:27 +0000

    This may be a bad idea, moving the modern version to 'Log4JLogSystem', but lets see who salutes
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/trunk@75848 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/velocity/runtime/log/Log4JLogSystem.java
@@ -0,0 +1,224 @@
+package org.apache.velocity.runtime.log;
+
+/*
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2001-2003 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Velocity", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+import java.util.Enumeration;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.RollingFileAppender;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.Level;
+import org.apache.log4j.Appender;
+
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.RuntimeServices;
+
+/**
+ * Implementation of a simple log4j system that will either latch onto
+ * an existing category, or just do a simple rolling file log.
+ *
+ * This is the one that you wan to use - it uses the modern 'Logger'
+ * concept of Log4j rather than the deprecated 'Categeory' concept.
+ *
+ * @author <a href="mailto:geirm@apache.org>Geir Magnusson Jr.</a>
+ * @author <a href="mailto:dlr@finemaltcoding.com>Daniel L. Rall</a>
+ * @version $Id: Log4JLogSystem.java,v 1.10 2003/10/22 10:49:27 geirm Exp $
+ * @since Velocity 1.3
+ */
+public class Log4JLogSystem implements LogSystem
+{
+    public static final String RUNTIME_LOG_LOG4J_LOGGER =
+            "runtime.log.logsystem.log4j.logger";
+
+    private RuntimeServices rsvc = null;
+
+    /**
+     * <a href="http://jakarta.apache.org/log4j/">Log4J</a>
+     * logging API.
+     */
+    protected Logger logger = null;
+
+    /**
+     * <a href="http://jakarta.apache.org/log4j/">Log4J</a>
+     * logging API.
+     */
+    public Log4JLogSystem()
+    {
+    }
+
+    public void init(RuntimeServices rs)
+    {
+        rsvc = rs;
+
+        /*
+         *  first see if there is a category specified and just use that - it allows
+         *  the application to make us use an existing logger
+         */
+
+        String loggerName =
+            (String) rsvc.getProperty(RUNTIME_LOG_LOG4J_LOGGER);
+
+        if (loggerName != null)
+        {
+            logger = Logger.getLogger(loggerName);
+        
+            logVelocityMessage(0,
+                               "SimpleLog4JLogSystem using logger '"
+                               + loggerName + '\'');
+
+            return;
+        }
+        
+        /*
+         *  if not, use the file...
+         */
+
+        String logfile = rsvc.getString(RuntimeConstants.RUNTIME_LOG);
+
+        /*
+         *  now init.  If we can't, panic!
+         */
+        try
+        {
+            internalInit(logfile);
+
+            logVelocityMessage(0,
+                "Log4JLogSystem initialized using logfile '" + logfile + "'");
+        }
+        catch(Exception e)
+        {
+            System.out.println(
+                "PANIC : error configuring Log4JLogSystem : " + e);
+        }
+    }
+
+    /**
+     *  initializes the log system using the logfile argument
+     */
+    private void internalInit(String logfile)
+        throws Exception
+    {
+        /*
+         *  do it by our classname to avoid conflicting with anything else 
+         *  that might be used...
+         */
+
+        logger = Logger.getLogger(this.getClass().getName());
+        logger.setAdditivity(false);
+
+        /*
+         * Priority is set for DEBUG becouse this implementation checks 
+         * log level.
+         */
+        logger.setLevel(Level.DEBUG);
+
+        RollingFileAppender appender = new RollingFileAppender(
+                new PatternLayout( "%d - %m%n"), logfile, true);
+        
+        appender.setMaxBackupIndex(1);
+        
+        appender.setMaximumFileSize(100000);
+
+        logger.addAppender(appender);
+    }
+
+    /**
+     *  logs messages
+     *
+     *  @param level severity level
+     *  @param message complete error message
+     */
+    public void logVelocityMessage(int level, String message)
+    {
+        switch (level) 
+        {
+            case LogSystem.WARN_ID:
+                logger.warn(message);
+                break;
+            case LogSystem.INFO_ID:
+                logger.info(message);
+                break;
+            case LogSystem.DEBUG_ID:
+                logger.debug(message);
+                break;
+            case LogSystem.ERROR_ID:
+                logger.error(message);
+                break;
+            default:
+                logger.debug(message);
+                break;
+        }
+    }
+
+    /**
+     * Also do a shutdown if the object is destroy()'d.
+     */
+    protected void finalize()
+            throws Throwable
+    {
+        shutdown();
+    }
+
+    /** Close all destinations*/
+    public void shutdown()
+    {
+        Enumeration appenders = logger.getAllAppenders();
+        while (appenders.hasMoreElements())
+        {
+            Appender appender = (Appender)appenders.nextElement();
+            appender.close();
+        }
+    }
+}
