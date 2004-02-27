c4438b0a0db246be4fa3321e044f36d7a6e60eab
Daniel Rall <dlr@apache.org>
2004-02-27 18:43:22 +0000

    Incorporated ASF license v2.  Thanks to Jeff Painter for the patch.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/trunk@75949 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/temporary/enhydra-velocity/example/presentation/WelcomePresentation.java
+++ contrib/temporary/enhydra-velocity/example/presentation/WelcomePresentation.java
@@ -1,4 +1,20 @@
 
+/*
+ * Copyright 2000-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // Enhydra
 import com.lutris.appserver.server.httpPresentation.*;
 
@@ -15,7 +31,6 @@ import java.text.DateFormat;
 /**
  * Simple example of how to use for presentation.
  *
- * @author <a href="mailto:daveb@miceda-data.com">Dave Bryson</a>
  * @version 1.0
  * @since 1.0
  * @see VelocityPO
