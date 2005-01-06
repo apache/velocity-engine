b8179a6bec112b71420844d534f294fbe709c85e
William Glass-Husain <wglass@apache.org>
2005-01-06 14:28:59 +0000

    Alow Velocity to be configured with an instance of a resource loader instead of passing in a class name.  Also added a setter for the DataSource to DataSourceResourceLoader. The upshot is that Velocity is now more friendly to Inversion of Control (IOC) implementations.  Thanks to Matt Raible for proposing this and testing it out.
    http://issues.apache.org/bugzilla/show_bug.cgi?id=28611
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/trunk@124381 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/velocity/test/ResourceLoaderInstanceTest.java
@@ -0,0 +1,141 @@
+package org.apache.velocity.test;
+
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+import org.apache.velocity.runtime.RuntimeSingleton;
+import org.apache.velocity.runtime.resource.loader.FileResourceLoader;
+import org.apache.velocity.runtime.resource.loader.ResourceLoader;
+
+/**
+ * Test that an instance of a ResourceLoader can be successfully passed in.
+ *
+ * @author <a href="mailto:wglass@apache.org">Will Glass-Husain</a>
+ * @version $Id: MultipleFileResourcePathTest.java,v 1.10 2004/03/19 17:13:38 dlr Exp $
+ */
+public class ResourceLoaderInstanceTest extends BaseTestCase
+{
+     /**
+     * VTL file extension.
+     */
+    private static final String TMPL_FILE_EXT = "vm";
+
+    /**
+     * Comparison file extension.
+     */
+    private static final String CMP_FILE_EXT = "cmp";
+
+    /**
+     * Comparison file extension.
+     */
+    private static final String RESULT_FILE_EXT = "res";
+
+    /**
+     * Path for templates. This property will override the
+     * value in the default velocity properties file.
+     */
+    private final static String FILE_RESOURCE_LOADER_PATH = "../test/resourceinstance";
+
+    /**
+     * Results relative to the build directory.
+     */
+    private static final String RESULTS_DIR = "../test/resourceinstance/results";
+
+    /**
+     * Results relative to the build directory.
+     */
+    private static final String COMPARE_DIR = "../test/resourceinstance/compare";
+
+    /**
+     * Default constructor.
+     */
+    ResourceLoaderInstanceTest()
+    {
+        super("ResourceLoaderInstanceTest");
+
+        try
+        {
+            assureResultsDirectoryExists(RESULTS_DIR);
+
+            ResourceLoader rl = new FileResourceLoader();
+            
+            // pass in an instance to Velocity
+            Velocity.addProperty( "resource.loader", "testrl" );
+            Velocity.setProperty( "testrl.resource.loader.instance", rl );
+            Velocity.setProperty( "testrl.resource.loader.path", FILE_RESOURCE_LOADER_PATH );
+
+            Velocity.init();
+        }
+        catch (Exception e)
+        {
+            System.err.println("Cannot setup ResourceLoaderInstanceTest!");
+            e.printStackTrace();
+            System.exit(1);
+        }            
+    }
+
+    public static junit.framework.Test suite ()
+    {
+        return new ResourceLoaderInstanceTest();
+    }
+
+    /**
+     * Runs the test.
+     */
+    public void runTest ()
+    {
+        try
+        {
+            Template template = RuntimeSingleton.getTemplate(
+                getFileName(null, "testfile", TMPL_FILE_EXT));
+                       
+            FileOutputStream fos = 
+                new FileOutputStream (
+                    getFileName(RESULTS_DIR, "testfile", RESULT_FILE_EXT));
+
+            
+            Writer writer = new BufferedWriter(new OutputStreamWriter(fos));
+            
+            /*
+             *  put the Vector into the context, and merge both
+             */
+
+            VelocityContext context = new VelocityContext();
+
+            template.merge(context, writer);
+            writer.flush();
+            writer.close();
+            
+            if ( !isMatch(RESULTS_DIR, COMPARE_DIR, "testfile",
+                    RESULT_FILE_EXT, CMP_FILE_EXT) )
+            {
+                fail("Output incorrect.");
+            }
+        }
+        catch (Exception e)
+        {
+            fail(e.getMessage());
+        }
+    }
+}
