62038af7803088f82fabe035f0aafc8a7838eda0
Daniel Rall <dlr@apache.org>
2005-06-21 23:03:49 +0000

    Added new directive.foreach.maxloops property which allows an upper bound on the number of times a #foreach loop will execute.
    Corrected inconsistent line endings throughout, and set svn:eol-style
    to native.
    
    
    * xdocs/user-guide.xml
    * xdocs/developer-guide.xml
    * xdocs/vtl-reference-guide.xml
    * docs/developer-guide.html
    * docs/vtl-reference-guide.html
    * docs/user-guide.html
      Added documentation blurb for directive.foreach.maxloops.
    
    
    * src/java/org/apache/velocity/test/ForeachTest.java
      New incarnation of specific whitebox testing of the #foreach
      directive.
    
      (setUp): Initialize Velocity with a max loop number of three.
    
      (testMaxNbrLoopsConstraint): New test which verifies limiting of the
       number of loop iterations.
    
      (testMethodCall): Code from ForeachMethodCallTestCase which Tests
       proper method execution during a Foreach loop with items of varying
       classes.
    
    
    * src/java/org/apache/velocity/test/ForeachMethodCallTestCase.java
      Renamed to ForeachTest.java.
    
    
    * src/java/org/apache/velocity/runtime/RuntimeConstants.java
      (MAX_NUMBER_LOOPS): New constant.
    
    
    * src/java/org/apache/velocity/runtime/defaults/velocity.properties
      (directive.foreach.maxloops): New property with a default of -1
       (meaning no limit).
    
    
    * src/java/org/apache/velocity/runtime/directive/Foreach.java
      (maxNbrLoops): New instance field which tracks the maximum number of
       times we're allowed to loop.
    
      (init): Initialize maxNbrLoops.
    
      (render): Leveraged maxNbrLoops to stop looping when the limit is
       reached.  Renamed ctr to savedCounter.
    
    
    * build/testcases.xml
      (test-all): Updated for rename of test-foreachmethodcall to
       test-foreach.
    
      (test-foreach): Renamed from test-foreachmethodcall, and accounted
       for rename of ForeachMethodCallTestCase to ForeachTest.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/core/trunk@191742 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/velocity/test/ForeachTest.java
@@ -0,0 +1,92 @@
+package org.apache.velocity.test;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+import org.apache.commons.collections.ExtendedProperties;
+
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.test.provider.ForeachMethodCallHelper;
+
+/**
+ * This class tests the Foreach loop.
+ *
+ * @author Daniel Rall
+ * @author <a href="mailto:wglass@apache.org">Will Glass-Husain</a>
+ */
+public class ForeachTest extends TestCase 
+{
+    private VelocityContext context;
+
+    public ForeachTest(String name)
+    {
+        super(name);
+    }
+
+    public void setUp()
+        throws Exception
+    {
+        // Limit the loop to three iterations.
+        Velocity.setProperty(RuntimeConstants.MAX_NUMBER_LOOPS,
+                             new Integer(3));
+        Velocity.init();
+
+        context = new VelocityContext();
+    }
+
+    /**
+     * Tests limiting of the number of loop iterations.
+     */
+    public void testMaxNbrLoopsConstraint()
+        throws Exception
+    {
+        StringWriter writer = new StringWriter();
+        String template = "#foreach ($item in [1..10])$item #end";
+        Velocity.evaluate(context, writer, "test", template);
+        assertEquals("Max number loops not enforced",
+                     "1 2 3 ", writer.toString());
+    }
+
+    /**
+     * Tests proper method execution during a Foreach loop with items
+     * of varying classes.
+     */
+    public void testMethodCall()
+        throws Exception
+    {
+        List col = new ArrayList();
+        col.add(new Integer(100));
+        col.add("STRVALUE");
+        context.put("helper", new ForeachMethodCallHelper());
+        context.put("col", col);
+
+        StringWriter writer = new StringWriter();
+        Velocity.evaluate(context, writer, "test",
+                          "#foreach ( $item in $col )$helper.getFoo($item) " +
+                          "#end");
+        assertEquals("Method calls while looping over varying classes failed",
+                     "int 100 str STRVALUE ", writer.toString());
+    }
+}
