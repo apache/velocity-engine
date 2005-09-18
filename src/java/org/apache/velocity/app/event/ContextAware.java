028cebb6988211d30a55d578197c69c122f7a4f8
William Glass-Husain <wglass@apache.org>
2005-09-18 05:33:20 +0000

    Added warning about thread safety
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/core/trunk@289890 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/velocity/app/event/ContextAware.java
+++ src/java/org/apache/velocity/app/event/ContextAware.java
@@ -23,6 +23,12 @@ import org.apache.velocity.context.Context;
  * have the method setContext called before each event.  This
  * allows the event handler to use information in the latest context
  * when responding to the event.
+ * 
+ * <P>Important Note: Only local event handlers attached to the context
+ * (as opposed to global event handlers initialized in the velocity.properties
+ * file) should implement ContextAware.  Since global event handlers are
+ * singletons individual requests will not be able to count on the 
+ * correct context being loaded before a request.
  *
  * @author <a href="mailto:wglass@wglass@forio.com">Will Glass-Husain</a>
  * @version $Id: GlobalEventHandler.java,v 1.4 2004/03/19 17:13:33 dlr Exp $
