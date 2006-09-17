31fa94636ab8913b2de700c24ccc0aacfb0c2e8b
Henning Schmiedehausen <henning@apache.org>
2006-09-17 11:31:25 +0000

    Add test cases for the DataSourceResourceLoader.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/engine/trunk@447048 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/velocity/test/sql/HsqlDataSource.java
@@ -0,0 +1,50 @@
+package org.apache.velocity.test.sql;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.hsqldb.jdbcDriver;
+
+public class HsqlDataSource implements DataSource {
+
+    private final String url;
+    
+    private PrintWriter logWriter = null;
+    
+    private int loginTimeout = 0;
+    
+    public HsqlDataSource(final String url) throws Exception {
+	this.url = url;
+	Class.forName(jdbcDriver.class.getName());
+    }
+    
+    public Connection getConnection() throws SQLException {
+	return DriverManager.getConnection(url, "sa", "");
+    }
+
+    public Connection getConnection(final String username, final String password)
+	    throws SQLException {
+	return DriverManager.getConnection(url, username, password);
+    }
+
+    public PrintWriter getLogWriter() throws SQLException {
+	return logWriter;
+    }
+
+    public int getLoginTimeout() throws SQLException {
+	return loginTimeout;
+    }
+
+    public void setLogWriter(PrintWriter logWriter) throws SQLException {
+	this.logWriter = logWriter;
+    }
+
+    public void setLoginTimeout(int loginTimeout) throws SQLException {
+	this.loginTimeout = loginTimeout;
+    }
+
+}
