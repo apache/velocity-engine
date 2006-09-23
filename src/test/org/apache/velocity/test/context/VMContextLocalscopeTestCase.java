7e4668de3b828851ceb105fa63816f1754edc680
William Glass-Husain <wglass@apache.org>
2006-09-23 23:43:43 +0000

    allow macros to get outside references, even with localscope setting.  Thanks to Stephen Habermann.  VELOCITY-459
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/engine/trunk@449333 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/velocity/test/context/VMContextLocalscopeTestCase.java
@@ -0,0 +1,64 @@
+package org.apache.velocity.test.context;
+
+/*
+ * Copyright 2001-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Tests scope of velocimacros with localscope setting. 
+ * 
+ * @author <a href="mailto:stephenh@chase3000.com">Stephen Habermann</a>
+ * @version $Id: Handler2.java 447976 2006-09-19 21:29:45Z henning $
+ */
+import junit.framework.TestCase;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.context.InternalContextAdapterImpl;
+import org.apache.velocity.context.VMContext;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.RuntimeInstance;
+
+public class VMContextLocalscopeTestCase extends TestCase {
+
+    private RuntimeInstance instance;
+
+    public void setUp() throws Exception 
+    {
+        this.instance = new RuntimeInstance();
+        this.instance.setProperty(RuntimeConstants.VM_CONTEXT_LOCALSCOPE, Boolean.TRUE);
+        this.instance.init();
+    }
+
+    public void testLocalscopePutDoesntLeakButGetDoes() 
+    {
+        VelocityContext base = new VelocityContext();
+        base.put("outsideVar", "value1");
+
+        VMContext vm = new VMContext(new InternalContextAdapterImpl(base), this.instance);
+        vm.put("newLocalVar", "value2");
+
+        // New variable put doesn't leak
+        assertNull(base.get("newLocalVar"));
+        assertEquals("value2", vm.get("newLocalVar"));
+
+        // But we can still get to "outsideVar"
+        assertEquals("value1", vm.get("outsideVar"));
+
+        // If we decide to try and set outsideVar it won't leak
+        vm.put("outsideVar", "value3");
+        assertEquals("value3", vm.get("outsideVar"));
+        assertEquals("value1", base.get("outsideVar"));
+    }
+
+}
