68abdc30c461456dcf4b8c6dcddd1979ff05029f
Nathan Bubna <nbubna@apache.org>
2008-08-20 21:12:31 +0000

    VELOCITY-439 add test case for Velocity[Engine].resourceExists(String)
    git-svn-id: https://svn.apache.org/repos/asf/velocity/engine/trunk@687467 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/velocity/test/ResourceExistsTestCase.java
@@ -0,0 +1,64 @@
+package org.apache.velocity.test;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.log.NullLogChute;
+import org.apache.velocity.runtime.resource.loader.FileResourceLoader;
+import org.apache.velocity.runtime.resource.loader.StringResourceLoader;
+
+/**
+ * Test the resource exists method
+ *
+ * @version $Id: ResourceExistsTestCase.java 687191 2008-08-19 23:02:41Z nbubna $
+ */
+public class ResourceExistsTestCase extends BaseTestCase
+{
+    private VelocityEngine velocity;
+
+    public ResourceExistsTestCase(String name)
+    {
+        super(name);
+    }
+
+    public void setUp() throws Exception
+    {
+        velocity = new VelocityEngine();
+        // pass in an instance to Velocity
+        velocity.addProperty("resource.loader", "myfile,string");
+        velocity.setProperty("myfile.resource.loader.instance", new FileResourceLoader());
+        velocity.setProperty("myfile.resource.loader.path", TEST_COMPARE_DIR + "/resourceexists");
+        velocity.setProperty("string.resource.loader.class", StringResourceLoader.class.getName());
+        velocity.setProperty(velocity.RUNTIME_LOG_LOGSYSTEM_CLASS, NullLogChute.class.getName());
+    }
+
+    public void testFileResourceExists() throws Exception
+    {
+        assertTrue(velocity.resourceExists("testfile.vm"));
+        assertFalse(velocity.resourceExists("nosuchfile.vm"));
+    }
+
+    public void testStringResourceExists() throws Exception
+    {
+        assertFalse(velocity.resourceExists("foo.vm"));
+        StringResourceLoader.getRepository().putStringResource("foo.vm", "Make it so!");
+        assertTrue(velocity.resourceExists("foo.vm"));
+    }
+}
