c24fd3138a1ca99ecab1e7e257ffce6b893e614e
Nathan Bubna <nbubna@apache.org>
2008-12-03 23:14:34 +0000

    merge trunk changes relevant to this branch
    git-svn-id: https://svn.apache.org/repos/asf/velocity/engine/branches/1.6.x@723123 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/velocity/util/introspection/UberspectImpl.java
+++ src/java/org/apache/velocity/util/introspection/UberspectImpl.java
@@ -430,8 +430,12 @@ public class UberspectImpl implements Uberspect, UberspectLoggable
             // if no values are being passed into the vararg
             if (actual.length == index)
             {
+                // copy existing args to new array
+                Object[] newActual = new Object[actual.length + 1];
+                System.arraycopy(actual, 0, newActual, 0, actual.length);
                 // create an empty array of the expected type
-                actual = new Object[] { Array.newInstance(type, 0) };
+                newActual[index] = Array.newInstance(type, 0);
+                actual = newActual;
             }
             // if one value is being passed into the vararg
             else if (actual.length == index + 1 && actual[index] != null)
