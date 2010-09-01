e079aeefffa96da45475af07dbf182f80ee22122
Nathan Bubna <nbubna@apache.org>
2010-09-01 19:13:16 +0000

    a more thorough $foreach test
    git-svn-id: https://svn.apache.org/repos/asf/velocity/engine/trunk@991659 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/velocity/test/issues/Velocity762TestCase.java
+++ src/test/org/apache/velocity/test/issues/Velocity762TestCase.java
@@ -37,4 +37,17 @@ public class Velocity762TestCase extends BaseTestCase
         assertEvalEquals("false false true ", template);
     }
 
+    public void testAllForeachProps()
+    {
+        String template = "#foreach( $number in [1..3] )"+
+                          "number:$number hasNext:$foreach.hasNext "+
+                           "first:$foreach.first last:$foreach.last "+
+                           "count:$foreach.count index:$foreach.index \n"+
+                          "#end";
+        String expect = "number:1 hasNext:true first:true last:false count:1 index:0 \n"+
+                        "number:2 hasNext:true first:false last:false count:2 index:1 \n"+
+                        "number:3 hasNext:false first:false last:true count:3 index:2 \n";
+        assertEvalEquals(expect, template);
+    }
+
 }
