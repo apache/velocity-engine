4498d46ef7d1b405f1f0d0b6e6cd47c62baa5aca
Sergiu Dumitriu <sdumitriu@apache.org>
2015-12-04 21:22:42 +0000

    VELOCITY-870: Exception displayed when trying to loop over an Iterable private class Fixed
    git-svn-id: https://svn.apache.org/repos/asf/velocity/engine/trunk@1718032 13f79535-47bb-0310-9956-ffa450edef68

--- velocity-engine-core/src/main/java/org/apache/velocity/util/introspection/UberspectImpl.java
+++ velocity-engine-core/src/main/java/org/apache/velocity/util/introspection/UberspectImpl.java
@@ -145,6 +145,10 @@ public class UberspectImpl implements Uberspect, UberspectLoggable
                     {
                         return (Iterator)iter.invoke(obj);
                     } 
+                    catch (IllegalAccessException e)
+                    {
+                        // Cannot invoke this method, just give up
+                    }
                     catch (Exception e)
                     {
                         throw new VelocityException("Error invoking the method 'iterator' on class '" 
